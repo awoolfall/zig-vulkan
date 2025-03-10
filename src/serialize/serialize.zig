7268c86200c3283015730a735a74a9167155d871
awoolfall <8043586+awoolfall@users.noreply.github.com>
2025-03-10 21:36:45 +1030

    serialize arrays and slices

--- src/serialize/serialize.zig
+++ src/serialize/serialize.zig
@@ -2,91 +2,112 @@ const std = @import("std");
 const builtin = @import("builtin");
 
 pub fn SerializableStruct(comptime T: type) type {
-    if (@typeInfo(T) != .@"struct") {
-        return T;
-    }
-
-    var fields: [std.meta.fields(T).len]std.builtin.Type.StructField = undefined;
-    for (std.meta.fields(T), 0..) |field, i| {
-        fields[i] = field;
-        switch (@typeInfo(field.type)) {
-            .@"struct" => |_| {
-                if (@hasDecl(field.type, "Serde")) {
-                    fields[i].type = SerializableStruct(field.type.Serde.T);
-                } else {
+    switch (@typeInfo(T)) {
+        .@"struct" => |s| {
+            if (@hasDecl(T, "Serde")) {
+                return SerializableStruct(T.Serde.T);
+            } else {
+                var fields: [std.meta.fields(T).len]std.builtin.Type.StructField = undefined;
+                for (std.meta.fields(T), 0..) |field, i| {
+                    fields[i] = field;
                     fields[i].type = SerializableStruct(field.type);
                 }
-            },
-            else => continue,
-        }
-    }
-
-    return @Type(.{
-        .@"struct" = .{
-            .layout = .auto,
-            .fields = &fields,
-            .decls = &[_]std.builtin.Type.Declaration{},
-            .is_tuple = false,
+                var new_s = s;
+                new_s.fields = &fields;
+                return @Type(.{
+                    .@"struct" = new_s,
+                });
+            }
         },
-    });
+        .@"array" => |a| {
+            var new_a = a;
+            new_a.child = SerializableStruct(a.child);
+            return @Type(.{
+                .@"array" = new_a,
+            });
+        },
+        .pointer => |p| {
+            std.debug.assert(p.size == .Many or p.size == .Slice);
+            var new_p = p;
+            new_p.child = SerializableStruct(p.child);
+            return @Type(.{
+                .pointer = new_p,
+            });
+        },
+        .bool, .int, .float, .comptime_int, .comptime_float, .null, .@"enum", .@"union", .vector => return T,
+        else => unreachable,
+    }
 }
 
 pub fn serialize(comptime T: type, allocator: std.mem.Allocator, value: T) !SerializableStruct(T) {
-    if (@typeInfo(T) != .@"struct") {
-        return value;
-    }
-
-    var serialized: SerializableStruct(T) = undefined;
-    const field_names = comptime std.meta.fieldNames(T);
-    inline for (field_names) |field_name| {
-        const v = blk: {
-            const field_type = @FieldType(T, field_name);
-            switch (@typeInfo(field_type)) {
-                .@"struct" => |_| {
-                    if (@hasDecl(field_type, "Serde")) {
-                        const s = try field_type.Serde.serialize(allocator, @field(value, field_name));
-                        const serializable_s = try serialize(field_type.Serde.T, allocator, s);
-                        break :blk serializable_s;
-                    } else {
-                        const serializable_s = try serialize(field_type, allocator, @field(value, field_name));
-                        break :blk serializable_s;
-                    }
-                },
-                else => break :blk @field(value, field_name),
+    switch (@typeInfo(T)) {
+        .@"struct" => |_| {
+            if (@hasDecl(T, "Serde")) {
+                const s = try T.Serde.serialize(allocator, value);
+                return try serialize(T.Serde.T, allocator, s);
+            } else {
+                var s: SerializableStruct(T) = undefined;
+                const field_names = comptime std.meta.fieldNames(T);
+                inline for (field_names) |field_name| {
+                    const v = try serialize(@FieldType(T, field_name), allocator, @field(value, field_name));
+                    @field(s, field_name) = v;
+                }
+                return s;
             }
-        };
-        @field(serialized, field_name) = v;
+        },
+        .@"array" => |a| {
+            var ar: SerializableStruct(T) = undefined;
+            for (0..a.len) |i| {
+                const v = try serialize(a.child, allocator, value[i]);
+                ar[i] = v;
+            }
+            return ar;
+        },
+        .pointer => |p| {
+            std.debug.assert(p.size == .Many or p.size == .Slice);
+            const ar = try allocator.alloc(SerializableStruct(p.child), value.len);
+            for (0..value.len) |i| {
+                ar[i] = try serialize(p.child, allocator, value[i]);
+            }
+            return ar;
+        },
+        else => return value,
     }
-
-    return serialized;
 }
 
 pub fn deserialize(comptime T: type, allocator: std.mem.Allocator, value: SerializableStruct(T)) !T {
-    if (@typeInfo(T) != .@"struct") {
-        return value;
-    }
-
-    var deserialized: T = undefined;
-    const field_names = comptime std.meta.fieldNames(T);
-    inline for (field_names) |field_name| {
-        const v = blk: {
-            const field_type = @FieldType(T, field_name);
-            switch (@typeInfo(field_type)) {
-                .@"struct" => |_| {
-                    if (@hasDecl(field_type, "Serde")) {
-                        const s = try deserialize(field_type.Serde.T, allocator, @field(value, field_name));
-                        const d = try field_type.Serde.deserialize(allocator, s);
-                        break :blk d;
-                    } else {
-                        const d = try deserialize(field_type, allocator, @field(value, field_name));
-                        break :blk d;
-                    }
-                },
-                else => break :blk @field(value, field_name),
+    switch (@typeInfo(T)) {
+        .@"struct" => |_| {
+            if (@hasDecl(T, "Serde")) {
+                const d = try deserialize(T.Serde.T, allocator, value);
+                return try T.Serde.deserialize(allocator, d);
+            } else {
+                var d: T = undefined;
+                const field_names = comptime std.meta.fieldNames(T);
+                inline for (field_names) |field_name| {
+                    const v = try deserialize(@FieldType(T, field_name), allocator, @field(value, field_name));
+                    @field(d, field_name) = v;
+                }
+                return d;
             }
-        };
-        @field(deserialized, field_name) = v;
+        },
+        .@"array" => |a| {
+            var ar: T = undefined;
+            for (0..a.len) |i| {
+                const v = try deserialize(a.child, allocator, value[i]);
+                ar[i] = v;
+            }
+            return ar;
+        },
+        .pointer => |p| {
+            std.debug.assert(p.size == .Many or p.size == .Slice);
+            const ar = try allocator.alloc(p.child, value.len);
+            for (0..value.len) |i| {
+                ar[i] = try deserialize(p.child, allocator, value[i]);
+            }
+            return ar;
+        },
+        else => return value,
     }
-
-    return deserialized;
 }
+
